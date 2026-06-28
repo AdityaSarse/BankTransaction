efb231621311c41f4591d19637f86e04d3e1e4f4
AdityaSarse <adityasarse362@gmail.com>
2026-06-28 23:32:14 +0530

    Creating Transaction Schema

---
+++ Backend/src/models/transaction.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const transactionSchema = new mongoose.Schema({
+    fromAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: true
+    },
+    toAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["SUCCESS", "FAILED", "PROCESSING", "REVERSED"],
+        default: "PROCESSING"
+    },
+    idemponencyKey: {
+        type: String,
+        unique: true,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+transactionSchema.index({ idemponencyKey: 1 })
+
+const transactionModel = mongoose.model("transaction", transactionSchema);
+module.exports = transactionModel;
