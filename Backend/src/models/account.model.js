1d678fd670330a9feb101898d6153dd5d4e82704
AdityaSarse <adityasarse362@gmail.com>
2026-06-28 23:55:49 +0530

    Aggregation pipeline for Balance

--- Backend/src/models/account.model.js
+++ Backend/src/models/account.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const ladgerModel = require("./ladger.model")
 const accountSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -25,6 +25,59 @@ const accountSchema = new mongoose.Schema({
 })
 
 accountSchema.index({ userId: 1, status: 1 })
+
+accountSchema.methods.getBalance = async function () {
+    const balanceData = await ladgerModel.aggregate([
+        {
+            $match: {
+                accountID: this._id
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalDebit: {
+                    $sum: {
+                        $cond: [
+                            { $eq: ["$type", "DEBIT"] },
+                            "$amount",
+                            0
+                        ]
+                    }
+                },
+                totalCredit: {
+                    $sum: {
+                        $cond: [
+                            { $eq: ["$type", "CREDIT"] },
+                            "$amount",
+                            0
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                balance: {
+                    $subtract: [
+                        "$totalCredit",
+                        "$totalDebit"
+                    ]
+                }
+            }
+        }
+    ])
+
+    if (balanceData.length === 0) {
+        return 0;
+    }
+
+    const totalDebit = balanceData[0].totalDebit || 0;
+    const totalCredit = balanceData[0].totalCredit || 0;
+
+    return totalCredit - totalDebit;
+
+}
 
 const accountModel = mongoose.model("account", accountSchema)
 
