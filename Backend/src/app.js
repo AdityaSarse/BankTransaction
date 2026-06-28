9ba3884b7e9d1d74f87ac2d12d696444fcd4e91d
AdityaSarse <adityasarse362@gmail.com>
2026-06-28 20:26:17 +0530

    Account route set up

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRouter = require("./routes/auth.route")
+const accountRouter = require("./routes/accounts.routes")
 const cookieparser = require("cookie-parser")
 
 
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieparser())
 app.use("/api/auth", authRouter);
+app.use("/api/v1/accounts", accountRouter);
 
 
 
