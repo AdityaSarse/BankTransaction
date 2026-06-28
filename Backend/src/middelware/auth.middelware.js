f3d77d856b9c479b8da7b20d8ca7458a78124ea7
AdityaSarse <adityasarse362@gmail.com>
2026-06-28 20:24:49 +0530

    MiddelWare for checking user is authorized

---
+++ Backend/src/middelware/auth.middelware.js
@@ -0,0 +1,40 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+
+
+async function authMiddelware(req, res, next) {
+
+    let token = req.cookies?.token;
+    if (!token && req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    try {
+        if (!token) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT)
+        const user = await userModel.findById(decoded.userId)
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            })
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+module.exports = {
+    authMiddelware
+}
