8a49812218038e3a4d682d1aac41443b83e772ef
AdityaSarse <adityasarse362@gmail.com>
2026-06-29 18:32:42 +0530

    Update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const authRouter = require("./routes/auth.route")
 const accountRouter = require("./routes/accounts.routes")
 const cookieparser = require("cookie-parser")
-
+const transactionRouter = require("./routes/transaction.route")
 
 const app = express();
 app.use(express.json())
 app.use(cookieparser())
 app.use("/api/auth", authRouter);
 app.use("/api/v1/accounts", accountRouter);
+app.use("/api/v1/transactions", transactionRouter);
+
 
 
 
