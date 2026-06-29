15d6f335dc77f8b64d632101db2cc5e354b810a6
AdityaSarse <adityasarse362@gmail.com>
2026-06-29 19:32:25 +0530

    Routes for fetch

--- Backend/src/routes/accounts.routes.js
+++ Backend/src/routes/accounts.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const { authMiddelware } = require("../middelware/auth.middelware");
-const { createAccount } = require("../controllers/account.controller");
+const { createAccount, getAccounts, getAccountById } = require("../controllers/account.controller");
 
 const router = express.Router();
 
-router.post("/", authMiddelware, createAccount)
+router.post("/", authMiddelware, createAccount);
+router.get("/", authMiddelware, getAccounts);
+router.get("/:accountId", authMiddelware, getAccountById);
 
-module.exports = router
+module.exports = router;
