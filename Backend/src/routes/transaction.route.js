262979b194297fb2007ab436d32a8e330092fbf3
AdityaSarse <adityasarse362@gmail.com>
2026-06-29 19:00:16 +0530

    Add systemUser route

--- Backend/src/routes/transaction.route.js
+++ Backend/src/routes/transaction.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createTransaction } = require("../controllers/transaction.controller");
-const { authMiddelware } = require("../middelware/auth.middelware")
+const { createTransaction, systemInitalFunds } = require("../controllers/transaction.controller");
+const { authMiddelware, adminMiddelware } = require("../middelware/auth.middelware")
 
 const router = express.Router();
 
 router.post("/transfer", authMiddelware, createTransaction);
+
+router.post("/systemInital-funds", adminMiddelware, systemInitalFunds)
+
 
 module.exports = router
